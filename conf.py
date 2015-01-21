a438b13ca18140f6cc380ab35d07e07eaa58bdfa
Deeman2014 <dklewis2011@gmail.com>
2015-01-21 07:58:19 -0500

    Update conf.py

--- conf.py
+++ conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'edX Installation'
-copyright = u'2015, DLewis'
+copyright = u'2015, Creative Commons Attribution-NonCommercial 4.0 International License'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
